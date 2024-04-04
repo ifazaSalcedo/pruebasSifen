69e971e02e435b7162905baeea08c928c5d323b9
Ignacio Salcedo <salcedo.ig82@gmail.com>
2024-04-04 19:47:41 -0400

    Init
    Iniciar prueba ws sifen Paraguay

---
+++ src/main/java/com/setsifen/PruebasSifenApplication.java
@@ -0,0 +1,14 @@
+package com.setsifen;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PruebasSifenApplication {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(PruebasSifenApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/setsifen/PruebasSifenApplicationTests.java
@@ -0,0 +1,13 @@
+package com.setsifen;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PruebasSifenApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
